374abfe9eb904afba03368687c15675e8fee6c10
AndriiKot <a_kocyuba@ukr.net>
2024-08-21 20:45:53 +0200

    __DOM__conspect ! optimization conspect   !

---
+++ __03__Optimization__/__004-01__input-type.js
@@ -0,0 +1,16 @@
+﻿// GOOD PRAXE
+const nameInputOptimized = document.createElement("input");
+nameInputOptimized.placeholder = "Product name";
+// !!!
+console.log(nameInputOptimized.type); // 'text'
+// !!!
+
+
+// !!! BAG PRAXE
+const nameInput = document.createElement("input");
+console.log(nameInput.type); // 'text'
+nameInput.type = "text"; // !!! no sense if 'text' is default value
+nameInput.placeholder = "Product name";
+// !!! BAG PRAXE end
+
+console.log(nameInput.type); // 'text'
